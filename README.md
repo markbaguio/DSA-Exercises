ee0aad7ff2232c8d23e3806856bf4ab58e4ce9a7
Mark Baguio <godwinbaguio@gmail.com>
2022-07-17 14:41:36 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DSA-Exercises
+
 This repository contains DSA exercises that I worked on.
+Test
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-for expense in range(len(expenses)):
-#     if(expenses[expense] == 2000):
-#         print("3.", "You have spent 2000 dollars.")
-#         break
